2c97d085a81ef6fe63c763c2f1cbba73bc33b5be
AbhayDwivedi3017 <abhaydwivedi3017@gmail.com>
2026-07-30 13:38:43 +0530

    style: cap logo image rendering height to prevent background overflow

--- src/components/KloudEraLogo.tsx
+++ src/components/KloudEraLogo.tsx
@@ -167,6 +167,7 @@ export const KloudEraLogo: React.FC<KloudEraLogoProps> = ({ className = "", icon
         alt="KloudEra Technologies"
         style={{ 
           height: iconOnly ? "44px" : logoHeight, 
+          maxHeight: "38px",
           width: "auto"
         }}
         className="object-contain"
